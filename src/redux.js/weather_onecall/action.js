8fc19555a33c01b03a93c8ea785de3d9f093a691
Navneet Kumar <navneetharsh3@gmail.com>
2022-06-27 18:24:19 +0530

    key updated

--- src/redux.js/weather_onecall/action.js
+++ src/redux.js/weather_onecall/action.js
@@ -25,14 +25,14 @@ export const get_location = (payload) => ({
 
 export const getOnecall = (lat, lon) => (dispatch) => {
     dispatch(weatherLoading())
-    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutelyalerts&units=metric&appid=64a1b2bf693c8827c861e064d96655e2`)
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutelyalerts&units=metric&appid=d07f44f393d328ed6b9bd3de4e36c2f8`)
         .then(res => dispatch(weatherOnecall(res.data)))
         .catch(error => dispatch(weather_error()));
 }
 
 export const getLocation = (lat, lon) => (dispatch) => {
     dispatch(weatherLoading());
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=64a1b2bf693c8827c861e064d96655e2`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d07f44f393d328ed6b9bd3de4e36c2f8`)
     .then(res=>dispatch(get_location(res.data.name)))
     .catch(error=>dispatch(weather_error()));
 }
